48937a5840b41637a9c6fca0b7a1e168e06cc31e
Jeremias O. Silva <jeremiasprogrammer@gmail.com>
2020-08-12 10:42:20 -0300

    feat(Teste): primeiro teste com selenium acessando site e clicando em um link.

---
+++ first-test/teste.py
@@ -0,0 +1,12 @@
+#C:\Users\Jeremias\Desktop\operadriver_win64
+
+from selenium import webdriver
+
+browser = webdriver.Chrome(executable_path=r"C:\chromeselenium\chromedriver.exe")
+
+browser.get("https://chartos.herokuapp.com")
+ 
+a_funcionarios = browser.find_element_by_xpath("/html/body/nav/div/ul/li[@class='nav-item']").click()
+
+print("era pra ter feito.")
+print(a_funcionarios)
